cb94e401b52b24d72dbecf90ed67ecd698e0a98e
Thanh Nguyen <tnansci@gmail.com>
2022-04-06 18:20:44 -0400

    initial commit

---
+++ src/main/java/teksystems/capstone/BoaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package teksystems.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaManagementSystemApplication.class, args);
+	}
+
+}
