7fc533c21620ff63e50783a58d01072c68ead270
Thanh Nguyen <tnansci@gmail.com>
2022-04-09 16:22:44 -0400

    Able to add a feeder into database

--- src/main/java/teksystems/capstone/controller/FeederController.java
+++ src/main/java/teksystems/capstone/controller/FeederController.java
@@ -3,10 +3,17 @@ package teksystems.capstone.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import teksystems.capstone.database.dao.FeederDAO;
+import teksystems.capstone.database.entity.Feeder;
+import teksystems.capstone.formbean.feeder.AddFeederFormBean;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -20,6 +27,66 @@ public class FeederController {
         ModelAndView response = new ModelAndView();
         response.setViewName("feeder/addFeeder");
 
+        return response;
+    }
+
+    @RequestMapping(value = "/feeder/added", method = {RequestMethod.GET})
+    public ModelAndView added(@Valid AddFeederFormBean form, BindingResult bindingResult) throws Exception {
+        ModelAndView response = new ModelAndView();
+
+        if (bindingResult.hasErrors()) {
+            for (ObjectError error : bindingResult.getAllErrors()) {
+                log.info(((FieldError) error).getField() + " " + error.getDefaultMessage());
+            }
+            response.addObject("bindingResult", bindingResult);
+
+            // if there is one or more errors we don't want to continue with the creating process,
+            // and show register.jsp page
+            response.setViewName("feeder/added");
+            return response;
+        }
+
+
+
+        // first, we assumed it's an edit, and thus we want to query the user from database using id
+        Feeder feeder = feederDAO.findById(form.getId());
+        // if user is null, aka the id isn't there, aka new user
+        if (feeder == null) {
+            // hence, create new user
+            feeder = new Feeder();
+        }
+
+        // to calculate age - not using right now
+//        Long ageDay = DAYS.between(form.getBirthDate(), LocalDate.now());
+//        log.info(ageDay.toString());
+//        String age;
+//        if (ageDay/365 > 0) {
+//            age = ageDay/365 + " years old";
+//        } else if (ageDay/30 > 0) {
+//            age = ageDay/30 + " months old";
+//        } else {
+//            age = ageDay + " days old";
+//        }
+//        List<String> ages = new ArrayList<>();
+//        ages.add(age);
+//        response.addObject("ages", ages);
+        log.info("info from From: "+form);
+        feeder.setName(form.getName());
+        feeder.setSize(form.getSize());
+        feeder.setStatus(form.getStatus());
+        feeder.setQuantity(form.getQuantity());
+
+        feederDAO.save(feeder);
+
+//        HashMap<Integer, Snake> map = new HashMap<>();
+//        for (Snake x:snakes) {
+//            map.put(x.getId(), x);
+//        }
+//        response.addObject("mapModel", map);
+
+        // if have time, add adding successful message
+        // use redirect to trigger the next method/function
+        response.setViewName("redirect:/snake/showSnakes");
         return response;
     }
 }
--- src/main/java/teksystems/capstone/database/dao/FeederDAO.java
+++ src/main/java/teksystems/capstone/database/dao/FeederDAO.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import teksystems.capstone.database.entity.Feeder;
-import teksystems.capstone.database.entity.Snake;
 
 import java.util.List;
 
 @Repository
 public interface FeederDAO extends JpaRepository<Feeder, Long> {
 
-    List<Snake> findByNameContainingIgnoreCase(@Param("name") String name);
+    Feeder findById(@Param("id") Integer id);
+
+    List<Feeder> findByNameContainingIgnoreCase(@Param("name") String name);
 }
